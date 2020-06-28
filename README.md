47de50832fd2da90d7e98d524099384949937e73
Omar ATARI <atari.omar@gmail.com>
2020-06-28 04:16:41 +0200

    Initialize basic skeleton

---
+++ README.md
@@ -0,0 +1,16 @@
+## Stack
+
+MERN stack for development (Mongo, Express, ReactJs, Node). Nginx for production build.
+
+
+
+## Getting Started
+
+```
+docker-compose up --build -d
+```
+
+## Build for production (UI & ReactJs)
+
+- replace Dockerfile content by Dockerfile_prod in ui folder
+- replace ui port service to 80 in docker-compose.yml
---
+++ ui/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+const apiUrl = `http://localhost:8080`;
+
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  async createUser() {
+    await axios.get(apiUrl + '/user-create');
+    this.loadUsers();
+  }
+
+  async loadUsers() {
+    const res = await axios.get(apiUrl + '/users');
+    this.setState({
+      users: res.data
+    });
+  }
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={() => this.createUser()}>Create User</button>
+          <p>Users list:</p>
+          <ul>
+            {this.state.users.map( (user, i) =>
+              <React.Fragment>
+                <li key={i}> {user._id}</li>
+                <li key={i}> {user.username}</li>
+                <li key={i}> {user.password}</li>
+                <li key={i}> {user.email}</li>
+                <p> {"\n"}</p>
+              </React.Fragment>
+            )}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
